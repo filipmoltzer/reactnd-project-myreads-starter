ef3fd72a882be546fe57d6bd1b2ca18eaf7c5b68
Filip-Moltzer <filip.moltzer@gmail.com>
2018-08-26 16:47:09 +0200

    Changed src name for easier understanding

--- src/App.js
+++ src/App.js
@@ -1,52 +1,50 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
+import StartPage from "./StartPage";
+import * as BooksAPI from "./BooksAPI";
+import "./App.css";
+import { Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
 
-import SearchPage from './SearchPage';
-import MainPage from './MainPage';
-
-import * as BooksAPI from './BooksAPI'
-import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-   books: []
-  }
+    books: []
+  };
 
   componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books: books })
-    })
+    BooksAPI.getAll().then(books => {
+      this.setState({ books: books });
+    });
   }
 
   moveShelf = (book, shelf) => {
     BooksAPI.update(book, shelf);
 
-  BooksAPI.getAll().then((books) => {
-    this.setState({ books: books })
-  })
-  }
-
+    BooksAPI.getAll().then(books => {
+      this.setState({ books: books });
+    });
+  };
 
   render() {
     return (
       <div className="app">
-      <Route exact path="/" render={() => (
-        <MainPage
-              books={this.state.books}
-              moveShelf={this.moveShelf}
-          /> )}
-          />
-
-      <Route path="/search" render={() => (
-        <SearchPage
-        moveShelf={this.moveShelf}
-        books={this.state.books}
-        /> )}
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <StartPage books={this.state.books} moveShelf={this.moveShelf} />
+          )}
         />
 
+        <Route
+          path="/search"
+          render={() => (
+            <SearchPage moveShelf={this.moveShelf} books={this.state.books} />
+          )}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
--- src/Book.js
+++ src/Book.js
@@ -1,34 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Book extends Component {
-  render () {
-    let displayedThumbnail = this.props.book.imageLinks ?
-    this.props.book.imageLinks.thumbnail :
-    '';
+  render() {
+    let displayedThumbnail = this.props.book.imageLinks // if
+      ? this.props.book.imageLinks.thumbnail // truthy = get the imageLinks
+      : ""; // Falsy = whitespace
 
     return (
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128,
-           height: 193, backgroundImage: `url("${displayedThumbnail}" ` }}></div>
-        <div className="book-shelf-changer">
-          <select
-          onChange={(event) => this.props.moveShelf(
-              this.props.book, event.target.value
-            )}
-            value={this.props.currentShelf}
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url("${displayedThumbnail}" ` // displayedThumbnail explained above
+            }}
+          />
+          <div className="book-shelf-changer">
+            <select
+              onChange={event =>
+                this.props.moveShelf(this.props.book, event.target.value)
+              }
+              value={this.props.currentShelf}
             >
-            <option value="move" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
+              <option value="move" disabled>
+                Move to...
+              </option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
         </div>
+        <div className="book-title">{this.props.book.title}</div>
+        <div className="book-authors">{this.props.book.authors}</div>
       </div>
-      <div className="book-title">{this.props.book.title}</div>
-      <div className="book-authors">{this.props.book.authors}</div>
-    </div>
     );
   }
 }
